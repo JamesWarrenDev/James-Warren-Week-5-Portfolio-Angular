b91262cf5e417e72d404b974fb3c421e8790eb38
JamesWarrenDev <112908847+JamesWarrenDev@users.noreply.github.com>
2022-09-30 16:57:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# James-Warren-Week-5-Portfolio-Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ub7gy3)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 390, name: 'Can Nguyen' },
+  { id: 214, name: 'Emilia Butu' },
+  { id: 314, name: 'Valeriy Pavlenko' },
+  { id: 315, name: 'Don Almeida' },
+  { id: 109, name: 'Muhammad Baqui' },
+  { id: 209, name: 'Irina Hashmi' },
+  { id: 341, name: 'Khondkar Islam' },
+  { id: 342, name: 'James Holdener' },
+  { id: 207, name: 'Erhan Uyar' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
